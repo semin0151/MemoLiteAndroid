7d8494bc37cd8049576a6b99a57eca16dbc1c01d
osemin <semin015123@gmail.com>
2024-12-03 20:22:11 +0900

    add app-ads.txt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.semin.memo"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

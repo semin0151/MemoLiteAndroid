226db127c65ab6fc799c6b3a4473df96491b6813
osemin <semin015123@gmail.com>
2024-12-05 21:15:56 +0900

    fix(MemoUpsertScreen) : fix upsert logic

--- app/src/main/java/com/semin/memo/presentation/memo/MemoUpsertScreen.kt
+++ app/src/main/java/com/semin/memo/presentation/memo/MemoUpsertScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.semin.memo.presentation.util.MemoSaveSuccessException
+import kotlin.math.min
 
 @Composable
 fun MemoUpsertScreen(
@@ -35,31 +36,72 @@ fun MemoUpsertScreen(
     memoContent: String,
     viewModel: MemoViewModel = hiltViewModel()
 ) {
+    val oldTitle = remember { memoTitle }
+    val oldContent = remember { memoContent }
     val title = remember { mutableStateOf(memoTitle) }
     val content = remember { mutableStateOf(memoContent) }
 
     if (backClicked) {
         LaunchedEffect(Unit) {
-            if (title.value.isNotEmpty() && content.value.isNotEmpty()) {
-                viewModel.upsertMemo(
-                    memoId = memoId,
-                    title = title.value,
-                    content = content.value
-                )
-                onShowSnackBar.invoke(MemoSaveSuccessException())
+            if (oldTitle != title.value || oldContent != content.value) {
+                if (content.value.isNotEmpty()) {
+                    if (title.value.isEmpty()) {
+                        viewModel.upsertMemo(
+                            memoId = memoId,
+                            title = content.value.substring(0..min(20, content.value.length.dec())),
+                            content = content.value
+                        )
+                    } else {
+                        viewModel.upsertMemo(
+                            memoId = memoId,
+                            title = title.value,
+                            content = content.value
+                        )
+                    }
+                    onShowSnackBar.invoke(MemoSaveSuccessException())
+                } else {
+                    if (title.value.isNotEmpty()) {
+                        viewModel.upsertMemo(
+                            memoId = memoId,
+                            title = title.value,
+                            content = content.value
+                        )
+                        onShowSnackBar.invoke(MemoSaveSuccessException())
+                    }
+                }
             }
+
             onBackClick.invoke()
         }
     }
 
     BackHandler(enabled = true) {
-        if (title.value.isNotEmpty() && content.value.isNotEmpty()) {
-            viewModel.upsertMemo(
-                memoId = memoId,
-                title = title.value,
-                content = content.value
-            )
-            onShowSnackBar.invoke(MemoSaveSuccessException())
+        if (oldTitle != title.value || oldContent != content.value) {
+            if (content.value.isNotEmpty()) {
+                if (title.value.isEmpty()) {
+                    viewModel.upsertMemo(
+                        memoId = memoId,
+                        title = content.value.substring(0..min(20, content.value.length.dec())),
+                        content = content.value
+                    )
+                } else {
+                    viewModel.upsertMemo(
+                        memoId = memoId,
+                        title = title.value,
+                        content = content.value
+                    )
+                }
+                onShowSnackBar.invoke(MemoSaveSuccessException())
+            } else {
+                if (title.value.isNotEmpty()) {
+                    viewModel.upsertMemo(
+                        memoId = memoId,
+                        title = title.value,
+                        content = content.value
+                    )
+                    onShowSnackBar.invoke(MemoSaveSuccessException())
+                }
+            }
         }
         onBackClick.invoke()
     }

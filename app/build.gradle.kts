129d4c2b680122833ebc1f348c895a65be23c72f
osemin <semin015123@gmail.com>
2024-12-07 23:18:16 +0900

    fix(build.gradle.kts) : update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.semin.memo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.4"
+        versionCode = 8
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

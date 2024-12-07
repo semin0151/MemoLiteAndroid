4f280e59b2739a6cace60a73e258557d9f4647ab
osemin <semin015123@gmail.com>
2024-12-07 22:48:09 +0900

    fix(MemoDetailScreen) : remove unnecessary params

--- app/src/main/java/com/semin/memo/presentation/memo/MemoDetailScreen.kt
+++ app/src/main/java/com/semin/memo/presentation/memo/MemoDetailScreen.kt
@@ -1,13 +1,10 @@
 package com.semin.memo.presentation.memo
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -21,17 +18,16 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.semin.memo.domain.memo.Memo
+import com.semin.memo.presentation.common.topbar.MemoDetailTopBar
 import com.semin.memo.presentation.util.MemoDeleteSuccessException
 
 @Composable
 fun MemoDetailScreen(
     modifier: Modifier = Modifier,
-    innerPadding: PaddingValues,
     onShowSnackBar: (Throwable?) -> Unit,
-    deleteClicked: Boolean,
-    editClicked: Boolean,
     onDeleteClick: () -> Unit,
     onEditClick: (Memo) -> Unit,
+    onBackClick: () -> Unit,
     memoId: Long,
     viewModel: MemoViewModel = hiltViewModel()
 ) {
@@ -41,25 +37,22 @@ fun MemoDetailScreen(
         viewModel.setCurrentMemoId(memoId)
     }
 
-    if (editClicked) {
-        LaunchedEffect(Unit) {
-            onEditClick.invoke(memo)
-        }
-    }
-
-    if (deleteClicked) {
-        LaunchedEffect(Unit) {
-            viewModel.deleteMemo()
-            onShowSnackBar.invoke(MemoDeleteSuccessException())
-            onDeleteClick.invoke()
-        }
-    }
-
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(innerPadding)
     ) {
+        MemoDetailTopBar(
+            onBackClick = onBackClick,
+            onEditClick = {
+                onEditClick.invoke(memo)
+            },
+            onDeleteClick = {
+                viewModel.deleteMemo()
+                onShowSnackBar.invoke(MemoDeleteSuccessException())
+                onDeleteClick.invoke()
+            }
+        )
+
         TextField(
             modifier = Modifier.fillMaxWidth(),
             value = memo.title,

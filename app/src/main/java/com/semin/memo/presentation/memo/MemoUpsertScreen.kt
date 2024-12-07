d28da4a7284d2ea031254e2e9aec6b8b1a0a8638
osemin <semin015123@gmail.com>
2024-12-07 22:47:53 +0900

    fix(MemoUpsertScreen) : remove unnecessary params

--- app/src/main/java/com/semin/memo/presentation/memo/MemoUpsertScreen.kt
+++ app/src/main/java/com/semin/memo/presentation/memo/MemoUpsertScreen.kt
@@ -2,17 +2,13 @@ package com.semin.memo.presentation.memo
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -21,15 +17,14 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.semin.memo.presentation.common.topbar.AddMemoTopBar
 import com.semin.memo.presentation.util.MemoSaveSuccessException
 import kotlin.math.min
 
 @Composable
 fun MemoUpsertScreen(
     modifier: Modifier = Modifier,
-    innerPadding: PaddingValues,
     onShowSnackBar: (Throwable?) -> Unit,
-    backClicked: Boolean,
     onBackClick: () -> Unit,
     memoId: Long,
     memoTitle: String,
@@ -40,40 +35,6 @@ fun MemoUpsertScreen(
     val oldContent = remember { memoContent }
     val title = remember { mutableStateOf(memoTitle) }
     val content = remember { mutableStateOf(memoContent) }
-
-    if (backClicked) {
-        LaunchedEffect(Unit) {
-            if (oldTitle != title.value || oldContent != content.value) {
-                if (content.value.isNotEmpty()) {
-                    if (title.value.isEmpty()) {
-                        viewModel.upsertMemo(
-                            memoId = memoId,
-                            title = content.value.substring(0..min(20, content.value.length.dec())),
-                            content = content.value
-                        )
-                    } else {
-                        viewModel.upsertMemo(
-                            memoId = memoId,
-                            title = title.value,
-                            content = content.value
-                        )
-                    }
-                    onShowSnackBar.invoke(MemoSaveSuccessException())
-                } else {
-                    if (title.value.isNotEmpty()) {
-                        viewModel.upsertMemo(
-                            memoId = memoId,
-                            title = title.value,
-                            content = content.value
-                        )
-                        onShowSnackBar.invoke(MemoSaveSuccessException())
-                    }
-                }
-            }
-
-            onBackClick.invoke()
-        }
-    }
 
     BackHandler(enabled = true) {
         if (oldTitle != title.value || oldContent != content.value) {
@@ -109,8 +70,41 @@ fun MemoUpsertScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(innerPadding)
     ) {
+        AddMemoTopBar(
+            onBackClick = {
+                if (oldTitle != title.value || oldContent != content.value) {
+                    if (content.value.isNotEmpty()) {
+                        if (title.value.isEmpty()) {
+                            viewModel.upsertMemo(
+                                memoId = memoId,
+                                title = content.value.substring(0..min(20, content.value.length.dec())),
+                                content = content.value
+                            )
+                        } else {
+                            viewModel.upsertMemo(
+                                memoId = memoId,
+                                title = title.value,
+                                content = content.value
+                            )
+                        }
+                        onShowSnackBar.invoke(MemoSaveSuccessException())
+                    } else {
+                        if (title.value.isNotEmpty()) {
+                            viewModel.upsertMemo(
+                                memoId = memoId,
+                                title = title.value,
+                                content = content.value
+                            )
+                            onShowSnackBar.invoke(MemoSaveSuccessException())
+                        }
+                    }
+                }
+
+                onBackClick.invoke()
+            }
+        )
+
         TextField(
             modifier = Modifier.fillMaxWidth(),
             value = title.value,

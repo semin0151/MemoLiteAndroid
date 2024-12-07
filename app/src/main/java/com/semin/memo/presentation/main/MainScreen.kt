a5264d8aaa6970e62656c168a10e1aa15ad8aa79
osemin <semin015123@gmail.com>
2024-12-07 22:47:14 +0900

    fix(MainScreen) : replace drawer

--- app/src/main/java/com/semin/memo/presentation/main/MainScreen.kt
+++ app/src/main/java/com/semin/memo/presentation/main/MainScreen.kt
@@ -1,22 +1,8 @@
 package com.semin.memo.presentation.main
 
-import androidx.activity.compose.BackHandler
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -26,11 +12,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import com.google.android.gms.ads.nativead.NativeAd
 import com.semin.memo.presentation.ads.CallNativeAd
 import com.semin.memo.presentation.ads.loadNativeAd
-import com.semin.memo.presentation.common.topbar.TopBar
 import com.semin.memo.presentation.util.SnackBarException
 import kotlinx.coroutines.launch
 
@@ -49,60 +33,12 @@ internal fun MainScreen(
             }
         }
     }
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val backPressedDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
 
-    BackHandler(enabled = true) {
-        if (drawerState.isOpen) {
-            coroutineScope.launch {
-                drawerState.close()
-            }
-        } else {
-            backPressedDispatcher?.onBackPressed()
-        }
-    }
-
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        gesturesEnabled = false,
-        drawerContent = {
-            ModalDrawerSheet {
-                Column(
-                    modifier = Modifier.fillMaxWidth(0.8F)
-                ) {
-                    Text(
-                        "MemoLite",
-                        modifier = Modifier
-                            .background(MaterialTheme.colorScheme.primaryContainer)
-                            .padding(16.dp)
-                            .fillMaxWidth()
-                    )
-                    HorizontalDivider(modifier = Modifier.fillMaxWidth())
-                    NavigationDrawerItem(
-                        label = { Text(text = "Default") },
-                        selected = false,
-                        onClick = {
-                            coroutineScope.launch {
-                                drawerState.close()
-                            }
-                        }
-                    )
-                    HorizontalDivider(modifier = Modifier.fillMaxWidth())
-                }
-            }
-        }
-    ) {
-        MainScreenContent(
-            navigator = navigator,
-            onShowSnackBar = onShowSnackBar,
-            onDrawerOpen = {
-                coroutineScope.launch {
-                    drawerState.open()
-                }
-            },
-            snackBarHostState = snackBarHostState
-        )
-    }
+    MainScreenContent(
+        navigator = navigator,
+        onShowSnackBar = onShowSnackBar,
+        snackBarHostState = snackBarHostState
+    )
 }
 
 @Composable
@@ -110,12 +46,8 @@ private fun MainScreenContent(
     modifier: Modifier = Modifier,
     navigator: MainNavigator,
     onShowSnackBar: (Throwable?) -> Unit,
-    onDrawerOpen: () -> Unit,
     snackBarHostState: SnackbarHostState
 ) {
-    val backClicked = remember { mutableStateOf(false) }
-    val editClicked = remember { mutableStateOf(false) }
-    val deleteClicked = remember { mutableStateOf(false) }
     var nativeAd by remember { mutableStateOf<NativeAd?>(null) }
     val context = LocalContext.current
 
@@ -127,16 +59,6 @@ private fun MainScreenContent(
 
     Scaffold(
         modifier = modifier,
-        topBar = {
-            TopBar(
-                modifier = modifier,
-                navigator = navigator,
-                onDrawerOpen = { onDrawerOpen.invoke() },
-                onBackClick = { backClicked.value = true },
-                onEditClick = { editClicked.value = true },
-                onDeleteClick = { deleteClicked.value = true }
-            )
-        },
         bottomBar = {
             if(nativeAd != null) {
                 CallNativeAd(nativeAd!!)
@@ -146,21 +68,7 @@ private fun MainScreenContent(
             MainNavHost(
                 navigator = navigator,
                 innerPadding = innerPadding,
-                onShowSnackBar = onShowSnackBar,
-                backClicked = backClicked.value,
-                deleteClicked = deleteClicked.value,
-                editClicked = editClicked.value,
-                onBackClick = {
-                    navigator.popBackStack()
-                    backClicked.value = false
-                },
-                onDeleteClick = {
-                    navigator.popBackStack()
-                    deleteClicked.value = false
-                },
-                onEditClick = {
-                    editClicked.value = false
-                }
+                onShowSnackBar = onShowSnackBar
             )
         },
         snackbarHost = { SnackbarHost(snackBarHostState) }

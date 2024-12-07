12395d4312883840fa4ccbf06c6e378d59e9db46
osemin <semin015123@gmail.com>
2024-12-07 21:58:09 +0900

    fix(build.gradle.kts) : add firebase crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
 }
 
 android {

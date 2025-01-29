b909ed3349a697f48cc077a4a8f73f71195f3edc
osemin <semin015123@gmail.com>
2025-01-29 17:56:02 +0900

    feat : add folder db

--- app/src/main/java/com/semin/memo/data/database/MemoDatabase.kt
+++ app/src/main/java/com/semin/memo/data/database/MemoDatabase.kt
@@ -2,15 +2,18 @@ package com.semin.memo.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.semin.memo.data.database.dao.FolderDao
 import com.semin.memo.data.database.dao.MemoDao
+import com.semin.memo.data.database.model.FolderEntity
 import com.semin.memo.data.database.model.MemoEntity
 
 @Database(
-    entities = [MemoEntity::class],
+    entities = [MemoEntity::class, FolderEntity::class],
     version = 1,
     autoMigrations = [],
     exportSchema = true
 )
 abstract class MemoDatabase : RoomDatabase() {
     abstract fun memoDao(): MemoDao
+    abstract fun folderDao(): FolderDao
 }

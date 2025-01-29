aa215a6f750caa6c12f6fe53a48b3a1688048b4c
osemin <semin015123@gmail.com>
2025-01-29 16:29:51 +0900

    chore : remove test code on MainActivity.kt

--- app/src/main/java/com/semin/memo/presentation/main/MainActivity.kt
+++ app/src/main/java/com/semin/memo/presentation/main/MainActivity.kt
@@ -64,19 +64,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MemoTheme {
-        Greeting("Android")
-    }
-}
